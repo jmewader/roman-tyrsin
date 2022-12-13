40565b2fd332e4038e1073a2d3a90dc6d43b1834
Настя <smelw@list.ru>
2022-12-13 17:39:40 +0300

    add slider

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$('.slider').slick({
+  infinite: true,
+  dots: false,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  prevArrow: "<img src='../img/arrow-left.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='../img/arrow-right.svg' class='next' alt='2'>",
+});
