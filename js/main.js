09b6bd89df28a87b7c2662a41b1782efbe6bce72
Настя <smelw@list.ru>
2022-12-14 14:48:21 +0300

    add new blocks

--- js/main.js
+++ js/main.js
@@ -3,6 +3,6 @@ $('.slider').slick({
   dots: false,
   slidesToShow: 3,
   slidesToScroll: 1,
-  prevArrow: "<img src='../img/arrow-left.svg' class='prev' alt='1'>",
-  nextArrow: "<img src='../img/arrow-right.svg' class='next' alt='2'>",
+  prevArrow: "<img src='../img/icons/arrow-left.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='../img/icons/arrow-right.svg' class='next' alt='2'>",
 });
