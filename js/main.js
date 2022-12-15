93943c19e6e249fbdfe0bf01a88d6890b6bd52b3
Настя <smelw@list.ru>
2022-12-15 16:18:00 +0300

    add mobile menu

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-$('.slider').slick({
+$(".slider").slick({
   infinite: true,
   dots: false,
   slidesToShow: 3,
@@ -6,3 +6,9 @@ $('.slider').slick({
   prevArrow: "<img src='../img/icons/arrow-right.svg' class='prev' alt='1'>",
   nextArrow: "<img src='../img/icons/arrow-right.svg' class='next' alt='2'>",
 });
+
+//menu-mobile
+document.querySelector(".second-button").addEventListener("click", function () {
+  document.querySelector(".animated-icon2").classList.toggle("open");
+});
+
