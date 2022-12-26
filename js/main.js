5bb5c614283256f69285373ace42de450b4bb6b5
Настя <smelw@list.ru>
2022-12-26 18:28:01 +0300

    add modal

--- js/main.js
+++ js/main.js
@@ -11,58 +11,61 @@ $(".slider").slick({
       settings: {
         slidesToShow: 2,
         slidesToScroll: 2,
-      }
+      },
     },
     {
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
+        slidesToScroll: 1,
+      },
+    },
+  ],
 });
 
 //tabs
-const tabsItem = document.querySelectorAll('.tabs__item');
-const tabsContent = document.querySelectorAll('.tabs__content-item')
+const tabsItem = document.querySelectorAll(".tabs__item");
+const tabsContent = document.querySelectorAll(".tabs__content-item");
 
 function getTabs() {
   tabsItem.forEach((item) => {
-    item.addEventListener('click', () => {
+    item.addEventListener("click", () => {
+      tabsContent.forEach((item) => {
+        item.classList.add("hidden");
+      });
 
-      tabsContent.forEach((item)=> {
-        item.classList.add('hidden')
-      })
-
-
-      const content = document.querySelector('#' + item.dataset.tab)
-      console.log(content)
-      content.classList.remove('hidden')
-    })
-  })
+      const content = document.querySelector("#" + item.dataset.tab);
+      console.log(content);
+      content.classList.remove("hidden");
+    });
+  });
 }
 
-getTabs()
+getTabs();
 
 /*open modal*/
-const body = document.querySelector('body');
-const modal = document.getElementById("modal");
-const btn = document.getElementById("openModal");
-const closeBtn = document.getElementsByClassName("modal__close")[0];
 
-btn.onclick = function() {
-  modal.style.display = "block";
-  body.style.overflow = "hidden";
-}
+function openModal() {
+  const body = document.querySelector("body");
+  const modal = document.getElementById("modal");
+  const btn = document.getElementById("openModal");
+  const closeBtn = document.getElementsByClassName("modal__close")[0];
 
-closeBtn.onclick = function() {
-  modal.style.display = "none";
-  body.style.overflow = "auto";
-}
+  btn.onclick = function () {
+    modal.style.display = "block";
+    body.style.overflow = "hidden";
+  };
 
-window.onclick = function(event) {
-  if (event.target == modal) {
+  closeBtn.onclick = function () {
     modal.style.display = "none";
-  }
+    body.style.overflow = "auto";
+  };
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  };
 }
+
+openModal();
