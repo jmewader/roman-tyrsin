2c7b8326348da93d7d4b82c2d5acbeca68940a93
Настя <smelw@list.ru>
2022-12-29 18:27:09 +0300

    changed page

--- js/main.js
+++ js/main.js
@@ -109,3 +109,16 @@ function scrollToTop() {
     behavior: "smooth",
   });
 }
+
+
+/*кнопка basket*/
+
+function onScroll() {
+  window.addEventListener("scroll", function () {
+    let scroll = document.querySelector(".btn-basket");
+    scroll.classList.toggle("active", window.scrollY > 1);
+  });
+}
+
+onScroll()
+
