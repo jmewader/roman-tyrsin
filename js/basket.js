b611f475b58977d1aa882c8fcc00891d226ba8e0
Настя <smelw@list.ru>
2022-12-30 16:44:13 +0300

    add new block

--- js/basket.js
+++ js/basket.js
@@ -1,10 +1,2 @@
 /*кнопка basket*/
 
-function onScroll() {
-  window.addEventListener("scroll", function () {
-    let scroll = document.querySelector(".btn-basket");
-    scroll.classList.toggle("active", window.scrollY > 1);
-  });
-}
-
-onScroll();
--- js/main.js
+++ js/main.js
@@ -23,9 +23,21 @@ $(".slider").slick({
   ],
 });
 
+$(".slider-products").slick({
+  infinite: true,
+  dots: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow: "<img src='../img/icons/arrow-right.svg' class='prev' alt='1'>",
+  nextArrow: "<img src='../img/icons/arrow-right.svg' class='next' alt='2'>",
+});
+
+
 //tabs
 const tabsItem = document.querySelectorAll(".tabs__item");
 const tabsContent = document.querySelectorAll(".tabs__content-item");
+const tabContent = document.querySelectorAll(".item-home");
+// const btnsBack = document.querySelectorAll(".breadcrumbs__list-back");
 
 function getTabs() {
   tabsItem.forEach((item) => {
@@ -35,12 +47,23 @@ function getTabs() {
       });
 
       const content = document.querySelector("#" + item.dataset.tab);
-      console.log(content);
       content.classList.remove("hidden");
     });
   });
 }
 
+// const btnBackFunc = () => {
+//   btnsBack.forEach((item) => {
+//     item.addEventListener("click", () => {
+
+//       tabsItem.forEach((item) => {
+
+//         const content = document.querySelector("#" + item.dataset.tab);
+
+//         content.classList.add("hidden");
+//       });
+//     });
+//   });
+// };
+
 getTabs();
-
-
